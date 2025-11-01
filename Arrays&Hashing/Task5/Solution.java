0d164442eb4e4154e58f4b1dac9296a9ae0d91ff
kuba <115719738+kubastra@users.noreply.github.com>
2025-11-01 19:00:36 +0100

    Update Solution.java

--- Arrays&Hashing/Task5/Solution.java
+++ Arrays&Hashing/Task5/Solution.java
@@ -11,8 +11,20 @@ class Solution {
     public List<String> decode(String str) {
         List<String> decoded = new ArrayList<>();
         int i = 0;
-        while(i < strs.length()){
-            # ...
+        while(i < str.length()){
+            int j = i;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i, j));
+
+            j++;
+
+            String word = str.substring(j, j + length);
+            decoded.add(word);
+
+            i = j + length;
         }
+        return decoded;
     }
 }
