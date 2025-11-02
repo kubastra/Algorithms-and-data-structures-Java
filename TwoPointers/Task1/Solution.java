42df1c55a4af0b08747403a67ae598f1a64a90b8
kuba <115719738+kubastra@users.noreply.github.com>
2025-11-02 13:59:11 +0100

    Create Solution.java

---
+++ TwoPointers/Task1/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int sum = 0;
+
+        while(left < right){
+            sum = numbers[left] + numbers[right];
+            if( sum > target){
+                right -= 1;
+                continue;
+            }else if(sum < target){
+                left += 1;
+            }else{
+                return new int[]{left + 1, right + 1};
+            }
+        }
+        return new int[0];
+    }
+}
